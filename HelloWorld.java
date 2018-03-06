9ddc9a8adb5241d382815625f4ac81f0538c4592
jialianglin <35917657+jialianglin@users.noreply.github.com>
2018-03-06 10:38:12 +0800

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,6 @@ package learn;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("hello world");
+		System.out.println("aaa");
 	}
-	
-	sysout out println
 }
